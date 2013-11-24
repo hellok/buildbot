7ef3e2fcd6cf5255a3f3cc0157a7c230cc753d03
hellok <hellok@hellokbuildbot.tsinghua.edu.cn>
2013-11-24 16:45:37 +0800

    seccomp

---
+++ embedded_interpreter/step1/config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "keescook@chromium.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "example"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "example 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "example"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://outflux.net/teach-seccomp/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
